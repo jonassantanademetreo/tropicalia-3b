541da9bcf37de81c43c3f3f3f622d0b33a036c02
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-04-01 08:24:01 -0300

    116

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded',function (){
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanohoAtualFonte += 0.1;
-        document.styleSheets.fontSize = `${tamanohoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanohoAtualFonte}rem`;
     })
     
 })
