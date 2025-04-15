d4cd1f1639f97fdd7fc14673b4fb9242225c0644
jonassantanademetreo <145785837+jonassantanademetreo@users.noreply.github.com>
2025-04-15 08:33:15 -0300

    oi

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-Acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -12,8 +23,4 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
-    
-
-
-
 });
